be9500a0498352266567df1fdb934c833debeba6
adammak91 <adam210125@o2.pl>
2022-02-03 09:36:07 +0100

    zadanie26

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let name;
+name="zmienna1";
+console.log("zmienna1");
+
+let name2;
+name2="zmienna2";
+console.log("zmienna2");
+
+let name3;
+name3="zmienna3";
+console.log("zmienna3");
+
+let zmienna1;
+zmienna1="1";
+console.log('1');
+
+let zmienna2;
+zmienna2='2';
+console.log('2');
+
+let zmienna3;
+zmienna3='3';
+console.log('3');
+
+let name4;
+name4="zmiennawszystko";
+console.log('zmiennawszystko');
+
+let zmiennawszystko;
+zmiennawszystko='zmienna1,zmienna2,zmienna3';
+console.log('zmienna1,zmienna2,zmienna3');
+
